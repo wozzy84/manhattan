fb3fe67f7ea6f31c1a0fa32e666c6d7540c8f895
Radek Molenda <radek.molenda@gmail.com>
2017-02-22 23:56:02 +0100

    Naive calculator implementation

--- week2/js/app.js
+++ week2/js/app.js
@@ -1,7 +1,29 @@
 $(function() {
-  $("#plus").on("click", function() {
-    var firstNumber = Number($("#first").val());
-    var secondNumber = Number($("#second").val());
-    $("#wynik").text(firstNumber + secondNumber);
+  var $input = $("#input-text"),
+      inputs = "";
+
+  $(".numbers button").on("click", function() {
+    var character = $(this).text(),
+        numberString = $input.val() + character;
+
+    $input.val(numberString);
+  })
+
+  $(".operations button.arithmetic").on("click", function() {
+    var operation = $(this).text();
+    inputs = $input.val() + operation;
+
+    $input.val("");
+  });
+
+  $(".operations button.result").on("click", function() {
+    var result = eval(inputs + $input.val());
+
+    $input.val(result);
+  });
+
+  $(".operations button.clear").on("click", function() {
+    inputs = "";
+    $input.val("");
   });
 });
