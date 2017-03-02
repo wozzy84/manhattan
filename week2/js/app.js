faed0a7d4bb042dc208dbaffa1a682fca33fcb4e
Piotr <piotrw84@gmail.com>
2017-03-02 15:51:18 +0100

    możliwość dodawania znaków bezpośrednio z klawiatury

--- week2/js/app.js
+++ week2/js/app.js
@@ -1,29 +1,48 @@
 $(function() {
   var $input = $("#input-text"),
-      inputs = "";
+           inputs = "";
 
   $(".numbers button").on("click", function() {
     var character = $(this).text(),
         numberString = $input.val() + character;
 
-    $input.val(numberString);
+    $input.val(numberString).focus();
+
   })
 
   $(".operations button.arithmetic").on("click", function() {
     var operation = $(this).text();
     inputs = $input.val() + operation;
 
-    $input.val("");
+    $input.val("").focus();
   });
 
   $(".operations button.result").on("click", function() {
     var result = eval(inputs + $input.val());
 
-    $input.val(result);
+    $input.val(result).focus();
   });
 
   $(".operations button.clear").on("click", function() {
     inputs = "";
-    $input.val("");
+    $input.val("").focus();
+  }); 
+
+
+  $(document).on("keypress", function(e) {
+  if(e.shiftKey && (e.which == 75)) {$input.val("")};
   });
+
+
+  $(document).on("keypress", function(e) {
+  if((e.which == 99)) {$input.val("")};
+  });
+
+
+  $(document).on("keypress", function(e) {
+  if((e.which == 61)) {var result = eval(inputs + $input.val());
+    $input.val(result)};
+  });
+
+ 
 });
